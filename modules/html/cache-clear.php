0b3932e3b1a3f9f1b1cccb4133fdc5f2f789acb8
rufovS <serg.rufov2012@yandex.ru>
2022-06-17 20:08:48 +0300

    Реализация очистки кеша

---
+++ modules/html/cache-clear.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Очистка кеша
+ */
+
+/** @var \Twig\Environment $twig */
+
+/**
+ * Удаление данных в директории
+ *
+ * @param string $path
+ * @return bool
+ */
+function Delete(string $path): bool
+{
+    if (is_dir($path) === true) {
+        $files = array_diff(scandir($path), array('.', '..', '.gitignore'));
+        foreach ($files as $file) {
+            Delete(realpath($path) . '/' . $file);
+        }
+        return @rmdir($path);
+    } else if (is_file($path) === true) {
+        return @unlink($path);
+    }
+    return false;
+}
+
+Delete(ROOT_DIR . '/cache/');
+
+$html = $twig->render('blank.twig', [
+    'title' => 'Очистка кеша',
+    'typeAlert' => 'success',
+    'description' => 'Очистка кеша успешна выполена'
+]);
+
+if (!DEBUG) {
+    echo Optimize::html($html);
+} else {
+    echo $html;
+}
+
