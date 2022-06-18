af3c0d92de38af943b0c75412f5bdcf3e5797ee7
rufovS <serg.rufov2012@yandex.ru>
2022-06-18 20:25:22 +0300

    DomPDF добавление Times New Roman

---
+++ lib/dompdf/lib/fonts/dompdf_font_family_cache.dist.php
@@ -0,0 +1,97 @@
+<?php
+return function ($rootDir) {
+    $distFontDir = $rootDir . '/lib/fonts';
+    return [
+        'sans-serif' =>
+            [
+                'normal' => $distFontDir . '/Helvetica',
+                'bold' => $distFontDir . '/Helvetica-Bold',
+                'italic' => $distFontDir . '/Helvetica-Oblique',
+                'bold_italic' => $distFontDir . '/Helvetica-BoldOblique'
+            ],
+        'times' =>
+            [
+                'normal' => $distFontDir . '/times',
+                'bold' => $distFontDir . '/timesbd',
+                'italic' => $distFontDir . '/timesi',
+                'bold_italic' => $distFontDir . '/timesbi'
+            ],
+        'times-roman' =>
+            [
+                'normal' => $distFontDir . '/times',
+                'bold' => $distFontDir . '/times',
+                'italic' => $distFontDir . '/times',
+                'bold_italic' => $distFontDir . '/times'
+            ],
+        'courier' =>
+            [
+                'normal' => $distFontDir . '/Courier',
+                'bold' => $distFontDir . '/Courier-Bold',
+                'italic' => $distFontDir . '/Courier-Oblique',
+                'bold_italic' => $distFontDir . '/Courier-BoldOblique'
+            ],
+        'helvetica' =>
+            [
+                'normal' => $distFontDir . '/Helvetica',
+                'bold' => $distFontDir . '/Helvetica-Bold',
+                'italic' => $distFontDir . '/Helvetica-Oblique',
+                'bold_italic' => $distFontDir . '/Helvetica-BoldOblique'
+            ],
+        'zapfdingbats' =>
+            [
+                'normal' => $distFontDir . '/ZapfDingbats',
+                'bold' => $distFontDir . '/ZapfDingbats',
+                'italic' => $distFontDir . '/ZapfDingbats',
+                'bold_italic' => $distFontDir . '/ZapfDingbats'
+            ],
+        'symbol' =>
+            [
+                'normal' => $distFontDir . '/Symbol',
+                'bold' => $distFontDir . '/Symbol',
+                'italic' => $distFontDir . '/Symbol',
+                'bold_italic' => $distFontDir . '/Symbol'
+            ],
+        'serif' =>
+            [
+                'normal' => $distFontDir . '/times',
+                'bold' => $distFontDir . '/timesbd',
+                'italic' => $distFontDir . '/timesi',
+                'bold_italic' => $distFontDir . '/timesbi'
+            ],
+        'monospace' =>
+            [
+                'normal' => $distFontDir . '/Courier',
+                'bold' => $distFontDir . '/Courier-Bold',
+                'italic' => $distFontDir . '/Courier-Oblique',
+                'bold_italic' => $distFontDir . '/Courier-BoldOblique'
+            ],
+        'fixed' =>
+            [
+                'normal' => $distFontDir . '/Courier',
+                'bold' => $distFontDir . '/Courier-Bold',
+                'italic' => $distFontDir . '/Courier-Oblique',
+                'bold_italic' => $distFontDir . '/Courier-BoldOblique'
+            ],
+        'dejavu sans' =>
+            [
+                'bold' => $distFontDir . '/DejaVuSans-Bold',
+                'bold_italic' => $distFontDir . '/DejaVuSans-BoldOblique',
+                'italic' => $distFontDir . '/DejaVuSans-Oblique',
+                'normal' => $distFontDir . '/DejaVuSans'
+            ],
+        'dejavu sans mono' =>
+            [
+                'bold' => $distFontDir . '/DejaVuSansMono-Bold',
+                'bold_italic' => $distFontDir . '/DejaVuSansMono-BoldOblique',
+                'italic' => $distFontDir . '/DejaVuSansMono-Oblique',
+                'normal' => $distFontDir . '/DejaVuSansMono'
+            ],
+        'dejavu serif' =>
+            [
+                'bold' => $distFontDir . '/DejaVuSerif-Bold',
+                'bold_italic' => $distFontDir . '/DejaVuSerif-BoldItalic',
+                'italic' => $distFontDir . '/DejaVuSerif-Italic',
+                'normal' => $distFontDir . '/DejaVuSerif'
+            ]
+    ];
+};
